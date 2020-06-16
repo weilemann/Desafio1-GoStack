87959d28e1cebafb33d1423d22a805f5c8bd2423
Yuri Weilemann <yuri_weilemann@hotmail.com>
2020-06-16 11:18:16 -0300

    Alterado método PUT

--- src/app.js
+++ src/app.js
@@ -27,10 +27,6 @@ app.put('/repositories/:id', (request, response) => {
   const { id } = request.params;
   const { title, url, techs } = request.body;
 
-  const repository = repositories.find((repository) => repository.id == id);
-
-  const { likes } = repository;
-
   const repositoryIndex = repositories.findIndex(
     (repository) => repository.id == id
   );
@@ -41,11 +37,14 @@ app.put('/repositories/:id', (request, response) => {
       .json({ error: 'Repository not found. Did you type the correct id?' });
   }
 
-  const updatedRepository = { id, title, url, techs, likes };
+  repositories[repositoryIndex] = {
+    ...repositories[repositoryIndex],
+    title: title ? title : repositories[repositoryIndex].title,
+    url: url ? url : repositories[repositoryIndex].url,
+    techs: techs ? techs : repositories[repositoryIndex].techs,
+  };
 
-  repositories[repositoryIndex] = updatedRepository;
-
-  return response.json(updatedRepository);
+  return response.json(repositories[repositoryIndex]);
 });
 
 app.delete('/repositories/:id', (request, response) => {
@@ -79,13 +78,12 @@ app.post('/repositories/:id/like', (request, response) => {
     return response
       .status(400)
       .json({ error: 'Repository not found. Did you type the correct id?' });
-  } else {
-    const updatedRepository = { id, title, url, techs, likes: likes + 1 };
-
-    repositories[repositoryIndex] = updatedRepository;
-
-    return response.json(updatedRepository);
   }
+  const updatedRepository = { id, title, url, techs, likes: likes + 1 };
+
+  repositories[repositoryIndex] = updatedRepository;
+
+  return response.json(updatedRepository);
 });
 
 module.exports = app;
